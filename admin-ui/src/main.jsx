1cff6146a644fcee8fa80e36be1e248fb0ca1dd8
pravin130794 <pravin130794@gmail.com>
2025-01-31 23:31:44 +0530

    added new custom font

--- admin-ui/src/main.jsx
+++ admin-ui/src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
+import theme from "./theme";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -11,7 +13,9 @@ root.render(
   <RecoilRoot>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </React.StrictMode>
   </RecoilRoot>
